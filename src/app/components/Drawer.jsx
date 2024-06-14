41121c8a73da8dd8194a90ae9c3d91fe1004e7b8
mr10us <minenkoduck@gmail.com>
2024-06-14 21:30:56 +0300

    fixed burger button

--- src/app/components/Drawer.jsx
+++ src/app/components/Drawer.jsx
@@ -4,19 +4,34 @@ import Link from "next/link";
 import { WhiteButton } from "./UI/Buttons/WhiteButton";
 import links from "@/app/nav.json";
 
-const Drawer = ({ isOpen, toggleDrawer }) => {
+/**
+ * Drawer component for the navigation menu.
+ *
+ * @param {Object} props - The component props.
+ * @param {boolean} props.isDrawerOpen - Whether the drawer is open or not.
+ * @param {Function} props.toggleDrawer - Function to toggle the drawer.
+ * @returns {JSX.Element} The Drawer component.
+ */
+const Drawer = ({ isDrawerOpen, toggleDrawer }) => {
+  // Reference to the drawer element.
   const drawerRef = useRef(null);
 
+  /**
+   * Update the drawer height based on its open state.
+   */
   useEffect(() => {
-    if (isOpen) {
+    // If the drawer is open, set the height to the scroll height plus 40px.
+    // Otherwise, set the height to 0.
+    if (isDrawerOpen) {
       drawerRef.current.style.maxHeight = `calc(${drawerRef.current.scrollHeight}px + 40px)`;
     } else {
       drawerRef.current.style.maxHeight = 0;
     }
-  }, [isOpen]);
+  }, [isDrawerOpen]);
 
   return (
     <div>
+      {/* The drawer element. */}
       <div
         ref={drawerRef}
         className="fixed top-[86px] left-0 w-full bg-[#546177] transition-max-height duration-300 overflow-hidden z-40"
@@ -24,18 +39,21 @@ const Drawer = ({ isOpen, toggleDrawer }) => {
       >
         <MainWrapper>
           <nav className="flex gap-4 flex-col py-5 pb-10">
+            {/* Map over the links array to render the links. */}
             {links.map(({ label, path }) => (
               <Link href={path} className="text-gray-200 w-fit py-2" key={path}>
                 {label}
               </Link>
             ))}
+            {/* Add a "Contact Us" button. */}
             <WhiteButton className="px-5 py-2 rounded-[30px] w-fit">
               Contact Us
             </WhiteButton>
           </nav>
         </MainWrapper>
       </div>
-      {isOpen && (
+      {/* If the drawer is open, render a backdrop to close the drawer. */}
+      {isDrawerOpen && (
         <div
           className="fixed top-[86px] left-0 w-full h-full bg-black bg-opacity-50 z-30"
           onClick={toggleDrawer}
--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -9,14 +9,26 @@ import Drawer from "../Drawer";
 import { WhiteButton } from "../UI/Buttons/WhiteButton";
 import { BurgerButton } from "../BurgerButton";
 
+/**
+ * The Header component is the top navigation bar of the website.
+ * It contains the logo, navigation links, and a "Contact Us" button.
+ * It also has a drawer for mobile devices.
+ */
 export const Header = () => {
+  // State variables to track the scroll position and the drawer state.
   const [isScrolled, setIsScrolled] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
+  /**
+   * Toggles the drawer state when the burger button is clicked.
+   */
   const toggleDrawer = () => {
-    setIsOpen(!isOpen);
+    setIsDrawerOpen(prev => !prev);
   };
 
+  /**
+   * Sets the scroll state based on the current scroll position.
+   */
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
@@ -31,49 +43,60 @@ export const Header = () => {
     };
   }, []);
 
+  /**
+   * Sets the body overflow based on the drawer state.
+   */
   useEffect(() => {
-    if (isOpen) {
+    if (isDrawerOpen) {
       document.body.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "auto";
     }
-  }, [isOpen]);
+  }, [isDrawerOpen]);
 
   return (
+    // The header element with the appropriate class names and styles.
     <header
-      className={`fixed top-0 left-0 w-full h-[86px] bg-gray-200 bg-opacity-5 transition-colors duration-300 z-50 ${
+      className={`fixed top-0 left-0 w-full h-[86px] bg-gray-200 ${isDrawerOpen ? "bg-[#273040] bg-opacity-100" : "bg-opacity-5"} transition-colors duration-300 z-50 ${
         isScrolled ? "bg-gray-950 bg-opacity-50" : ""
       }`}
     >
+      {/* The main wrapper for the header content. */}
       <MainWrapper className="flex items-center h-full">
         <div className="flex justify-between items-center w-full">
+          {/* The logo link. */}
           <Link href="#hero">
             <Image
+              className="h-auto"
               src="/logo.svg"
               width={33}
               height={70}
               alt="logo"
               priority
-              style={{ height: "auto" }}
             />
           </Link>
+          {/* The navigation links. */}
           <nav className="gap-4 items-center hidden head:flex">
             {links.map(({ label, path }) => (
               <Link href={path} className="text-gray-100 px-4 py-2" key={label}>
                 {label}
               </Link>
             ))}
+            {/* The "Contact Us" button. */}
             <WhiteButton style={{ padding: "10px 20px", borderRadius: 30 }}>
               Contact Us
             </WhiteButton>
           </nav>
+          {/* The burger button for mobile devices. */}
           <BurgerButton
+            isActive={isDrawerOpen}
             className="inline head:hidden"
             onClick={toggleDrawer}
           />
         </div>
       </MainWrapper>
-      <Drawer isOpen={isOpen} toggleDrawer={toggleDrawer} />
+      {/* The drawer component for mobile devices. */}
+      <Drawer isDrawerOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
     </header>
   );
 };

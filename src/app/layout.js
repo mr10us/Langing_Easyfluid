80722ab489ac4445a505004c40d68663e05b77ed
mr10us <minenkoduck@gmail.com>
2024-06-18 14:00:18 +0300

    SEO added:

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,18 +5,49 @@ import { cn } from "./utils";
 const heebo = Heebo({
   subsets: ["latin"],
   weight: ["200", "400", "500", "700"],
-  variable: "--heebo-font"
+  variable: "--heebo-font",
 });
 
 const inter = Inter({
   subsets: ["latin"],
   weight: ["300", "700"],
-  variable: "--inter-font"
+  variable: "--inter-font",
 });
 
 export const metadata = {
   title: "EasyFluid",
-  description: "EasyFluid is a web based service for monitoring your metalworking fluids, oils etc.",
+  description:
+    "EasyFluid is a web based service for monitoring your metalworking fluids, oils etc.",
+  viewport: "width=device-width, initial-scale=1 user-scalable=no",
+  robots: "noindex, nofollow",
+  icons: {
+    icon: "/favicon.png",
+    shortcut: ["/favicon-16x16.png", "/favicon-32x32.png"],
+    apple: "/apple-touch-icon.png",
+  },
+  themeColor: "#ffffff",
+  openGraph: {
+    title: "EasyFluid",
+    description:
+      "EasyFluid is a web based service for monitoring your metalworking fluids, oils etc.",
+    url: "https://easyfluid.com",
+    siteName: "EasyFluid",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    title: "EasyFluid",
+    description:
+      "EasyFluid is a web based service for monitoring your metalworking fluids, oils etc.",
+    card: "summary",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -24,7 +55,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
-      <body className={cn(heebo.variable, inter.variable, heebo.className)}>{children}</body>
+      <body className={cn(heebo.variable, inter.variable, heebo.className)}>
+        {children}
+      </body>
     </html>
   );
 }
